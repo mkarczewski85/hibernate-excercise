7310b043fb750902af438c864744443caf71193f
Lukasz <lukbacic@gmail.com>
2017-09-27 17:15:30 +0200

    Projekt

---
+++ src/ogloszenia/App.java
@@ -0,0 +1,14 @@
+package ogloszenia;
+
+import ogloszeniar.hibernate.util.HibernateUtil;
+
+/**
+ * Created by Lukasz on 27.09.2017.
+ */
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        HibernateUtil.openSession();
+
+    }
+}
