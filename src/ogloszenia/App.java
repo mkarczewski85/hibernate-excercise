832a759b2d29fe4df41b7be27c42b5530cc5fc0c
Maciej Karczewski <m.karczewski1985@gmail.com>
2017-09-29 20:46:16 +0200

    update

--- src/ogloszenia/App.java
+++ src/ogloszenia/App.java
@@ -1,9 +1,6 @@
 package ogloszenia;
 
-import ogloszenia.model.Cena;
-import ogloszenia.model.Kolor;
-import ogloszenia.model.Material;
-import ogloszenia.model.Zabawka;
+import ogloszenia.model.*;
 import ogloszenia.repository.ZabawkaRepository;
 import ogloszenia.repository.ZamowienieRepository;
 import ogloszeniar.hibernate.util.HibernateUtil;
@@ -12,10 +9,11 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -31,15 +29,21 @@ public class App {
 
         Path path = Paths.get("C:\\Projekty Maciej\\hibernate\\src\\ogloszenia\\toy.jpg");
         byte[] image = Files.readAllBytes(path);
+        Path path2 = Paths.get("C:\\Projekty Maciej\\hibernate\\src\\ogloszenia\\toy2.jpg");
+        byte[] image2 = Files.readAllBytes(path2);
 
         Path path_doc = Paths.get("C:\\Projekty Maciej\\hibernate\\src\\ogloszenia\\toy_report.pdf");
         byte[] document = Files.readAllBytes(path_doc);
 
         Cena cena = new Cena(new BigDecimal(34.56), "PLN");
-        Zabawka lalka = new Zabawka("Lalka Barbie", cena, Kolor.CZERWONY, 56, false, Material.PLASTIK, LocalDateTime.now(),
+        Cena cena2 = new Cena(new BigDecimal(46.84), "PLN");
+        Zabawka doll = new Zabawka("Lalka Barbie", cena, Kolor.CZERWONY, 56, false, Material.PLASTIK, LocalDateTime.now(),
                 image, document);
+        Zabawka teddyBear = new Zabawka("Miś Uszatek", cena2, Kolor.SZARY, 43, true, Material.PLUSZ, LocalDateTime.now(),
+                image2, document);
 
-        ZabawkaRepository.save(lalka);
+        ZabawkaRepository.save(doll);
+        ZabawkaRepository.save(teddyBear);
 
         List<Zabawka> toysList = ZabawkaRepository.findToyWithPriceLessThanPara(new BigDecimal(50));
 
@@ -52,8 +56,46 @@ public class App {
         //TWORZENIE ZAMOWIENIA
 
         HashMap<Zabawka, Integer> listaZakupow = new HashMap<>();
-        listaZakupow.put(toysList.stream().findAny().orElse(lalka), new Integer(5));
+        listaZakupow.put(toysList.stream().findAny().orElse(doll), new Integer(5));
         ZamowienieRepository.createOrder(listaZakupow, "test@wp.pl");
+
+        //SZUKANIE ZABAWKI O PODANYM ID
+
+        Optional<Zabawka> toy = ZabawkaRepository.findToy(1);
+
+        if (toy.isPresent()) {
+            logger.info("Zabawka z if: " + toy.get().getId());
+        }
+
+        toy.ifPresent(t -> logger.info("Zabawka (lambda consumer): " + toy.get().getId()));
+
+//        //USUWANIE ZABAWKI
+//        if (toy.isPresent()) {
+//            ZabawkaRepository.delete(toy.get());
+//        }
+
+        //UPDATE NA BAZIE
+        if (toy.isPresent()) {
+            toy.get().setNazwa("Lalka bez nazwy");
+            ZabawkaRepository.update(toy.get());
+        }
+
+        if (toy.isPresent()) {
+
+            List<Zamowienie> allOrdersbyToy = ZamowienieRepository.findAllOrdersbyToy(toy.get());
+            for (Zamowienie z : allOrdersbyToy) {
+                logger.info(z.getId() + " : " + z.getDataZamowienia());
+            }
+        }
+
+        logger.info(ZabawkaRepository.findToyWithNameAndAvaibility().get(0).toString());
+
+        ZabawkaRepository.updatePrice(new BigDecimal(100));
+
+        List<Zabawka> toyByNameLike = ZabawkaRepository.findToyByNameLike("toyByNameLike");
+        for (Zabawka z : toyByNameLike){
+            logger.info(z.getNazwa());
+        }
 
     }
 }
--- src/ogloszenia/repository/ZamowienieRepository.java
+++ src/ogloszenia/repository/ZamowienieRepository.java
@@ -5,10 +5,11 @@ import ogloszenia.model.Zabawka;
 import ogloszenia.model.Zamowienie;
 import ogloszeniar.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.mapping.Collection;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import javax.persistence.Query;
+import java.math.BigDecimal;
+import java.util.*;
 
 public class ZamowienieRepository {
 
@@ -21,7 +22,7 @@ public class ZamowienieRepository {
 
             toys.forEach((zabawka, liczba) -> pozycjaZamowieniaSet.add(new PozycjaZamowienia(liczba, zabawka, order)));
 
-            for (PozycjaZamowienia pz : pozycjaZamowieniaSet){
+            for (PozycjaZamowienia pz : pozycjaZamowieniaSet) {
                 session.save(pz);
             }
             return true;
@@ -35,4 +36,28 @@ public class ZamowienieRepository {
         }
 
     }
+
+    public static List<Zamowienie> findAllOrdersbyToy(Zabawka z) {
+        Session session = null;
+        try {
+            session = HibernateUtil.openSession();
+            String hql = "SELECT z FROM Zamowienie z " +
+                    "LEFT JOIN z.pozycjaZamowieniaSet pz" +
+                    " WHERE pz.zabawka.id = :id";
+
+            Query query = session.createQuery(hql, Zamowienie.class);
+            query.setParameter("id", z.getId());
+            return query.getResultList();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+    }
+
+
+
 }
